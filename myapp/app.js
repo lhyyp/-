a17516035054dd187632b057d8f05971e6db0335
Yuanping <836444329@qq.com>
2018-09-03 09:02:49 +0800

    111

--- myapp/app.js
+++ myapp/app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const multer=require('multer');
+const multerObj=multer({dest:'./public/upload'});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(multerObj.any());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
