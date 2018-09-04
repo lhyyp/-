e0ce84c046650da110f7a0b1029d077049ac6e61
Yuanping <836444329@qq.com>
2018-09-04 10:08:42 +0800

    eqwe

--- myapp/routes/api.js
+++ myapp/routes/api.js
@@ -9,7 +9,6 @@ const async =require("async")
 router.get('/getbanner',(req,res)=>{
     db.selectAll('product_banner',(data)=>{
     	res.json(data);
-
     });
 })
 
@@ -213,43 +212,6 @@ router.get('/reduce',(req,res)=>{
 
 
 
-
-
-router.get('/demo',(req,res)=>{
-  async.waterfall([
-      //A这件事
-      function(cb){
-          db.doSomething( function(err, dataA){
-              console.log(dataA);
-              cb(err, dataA);     //如果发生err， 则瀑布就完了，后续流程都不会执行，B和C都不会执行
-          });
-      },
-      //B这件事，dataA就是上一步cb(err, dataA)中传进来的dataA
-      function(dataA, cb){
-          db.doSomething(function(err, dataB){
-              console.log(dataB);
-              cb(err, dataA, dataB);  //如果发生err， 则瀑布就完了，后续流程都不会执行，C不会执行
-          });
-      },
-      //C这件事
-      function(dataA, dataB, cb){
-          db.doSomething(function(err, dataC){
-              console.log(dataC);
-              cb(err, dataA, dataB, dataC);
-          });
-      }
-  ], function (err, dataA, dataB, dataC) {    //瀑布的每一布，只要cb(err, data)的err发生，就会到这
-      if(err){
-          console.log('处理错误!');
-      }else{
-          console.log('处理成功！');
-          console.log(dataA);
-          console.log(dataB);
-          console.log(dataC);
-      }
-  });
-
-})
 
 
 
